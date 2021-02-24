895038a201db1aa0f3323171564ac214efd36d9d
권대선 <dsun@hanmail.net>
2021-02-24 14:47:22 +0900

    Initial commit

---
+++ src/Models/DefaultBrowserSearchModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Atomus.Windows.Browser.Models
+{
+    internal class DevExpressBrowserSearchModel : Mvc.Models
+    {
+        internal decimal MENU_ID { get; set; }
+        internal decimal ASSEMBLY_ID { get; set; }
+    }
+}
---
+++ src/View/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Atomus.Windows.Browser
+{
+    /// <summary>
+    /// App.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            //this.Run(new DefaultBrowser());
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            //this.StartupUri = new Uri("DefaultBrowser.xaml", UriKind.Relative);
+        }
+    }
+}
